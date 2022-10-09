7544cce421580ac05dc804e1088e48c296930de6
EricaThompson <EricaThompson.NC@gmail.com>
2022-10-09 11:23:45 -0400

    Add nav

---
+++ src/components/Homepage.js
@@ -0,0 +1,22 @@
+import MainStory from "./MainStory"
+
+function Homepage(){
+    return (
+        <div className="homepage">
+            <nav className="banner">
+                <div className="menu-icon">
+                    📋
+                </div>
+                <div className="title">
+                    🗞
+                </div>
+            </nav>
+            <MainStory />
+            
+            
+            
+        </div>
+    )
+}
+
+export default Homepage
