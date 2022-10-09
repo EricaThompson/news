d54d6f33006fc1576fcb125a8c2b60b73980d233
EricaThompson <EricaThompson.NC@gmail.com>
2022-10-09 11:24:04 -0400

    Add MainStory

---
+++ src/components/MainStory.js
@@ -0,0 +1,15 @@
+function MainStory(){
+    return (
+        <div className="main-story">
+            <div className="tag">Okay, okay..</div>
+            <div width="">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/UN_General_Assembly_hall.jpg"></img> 
+            </div>
+            <div className="credit"></div>  
+            <div className="headline"></div>
+            <div className="author"></div>  
+        </div>
+    )
+    }
+    
+    export default MainStory
