f6be5b2e9b8a672dacd4c8a2ef4fa8c2412ac5ea
EricaThompson <EricaThompson.NC@gmail.com>
2022-10-13 05:48:37 -0400

    Add more mini stories

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -9,16 +9,26 @@ function Headlines (){
             title: "This Hunter Biden deal in a foreign country really does look bad",
             from: "FRANK FIGLIUZZI / OPINION",
             type: "story"
-        }
+        },
+        {id: 3,
+            title:  "Some FBI agents see indictable tax case against Hunter Biden, Washington Post reports",
+            from: "THE BEAT WITH ARI",
+        type: "video"},
+            {
+                id:4,
+                title: "This Hunter Biden deal in a foreign country really does look bad",
+                from: "FRANK FIGLIUZZI / OPINION",
+                type: "story"
+            }
     ]
 
     let headlineMap = headlines.map((headline)=>{
         return(
-            <div key={headline.id}>
-                <div className="icon">
+            <div key={headline.id} className="mini-story">
+                <div className="mini-type">
                     {headline.type}
-                    <div className="headline">{headline.title}</div>
-                    <div className="from">{headline.from}</div>
+                    <div className="mini-headline">{headline.title}</div>
+                    <div className="mini-author">{headline.from}</div>
                 </div>            
             </div>
 
